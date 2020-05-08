58ffb829f0999422b390b2b196a86e8b263d9479
teng <wwwsousoucom@126.com>
2020-05-08 15:25:51 +0800

    screen

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+use mortal::{Color, Event, Key, PrepareConfig, Screen, Style};
+use std::{thread, time};
 trait Updater {
     fn update(&mut self);
 }
 
 trait Renderer {
-    fn render(&mut self);
+    fn render(&mut self, scrren: &Screen);
 }
 
 struct GameLoop<U, R>
@@ -13,6 +15,7 @@ where
 {
     updater: U,
     renderer: R,
+    screen: Screen,
 }
 
 impl<U, R> GameLoop<U, R>
@@ -20,9 +23,21 @@ where
     U: Updater,
     R: Renderer,
 {
+    pub fn new(updater: U, renderer: R) -> Self {
+        let screen = Screen::new(PrepareConfig {
+            ..PrepareConfig::default()
+        })
+        .unwrap();
+        Self {
+            updater,
+            renderer,
+            screen,
+        }
+    }
+
     fn tick(&mut self) {
         self.updater.update();
-        self.renderer.render();
+        self.renderer.render(&self.screen);
     }
 }
 
@@ -30,15 +45,17 @@ struct Update(usize);
 impl Updater for Update {
     fn update(&mut self) {
         self.0 += 1;
-        println!("hello {}", self.0);
     }
 }
 
 struct Render(usize);
 impl Renderer for Render {
-    fn render(&mut self) {
+    fn render(&mut self, screen: &Screen) {
         self.0 += 2;
-        println!("hello {}", self.0);
+        screen.clear_screen();
+        screen.write_at((0, 0), "Reading input. Press 'g' to stop.");
+        screen.refresh().unwrap();
+        thread::sleep(time::Duration::from_millis(100));
     }
 }
 
@@ -46,11 +63,7 @@ fn main() {
     let loops = 10;
     let render = Render(0);
     let update = Update(0);
-
-    let mut game_loop = GameLoop {
-        updater: update,
-        renderer: render,
-    };
+    let mut game_loop = GameLoop::new(update, render);
     for i in 0..loops {
         game_loop.tick();
     }
