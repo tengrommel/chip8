3e87d7b000f230a5e342bf278be66dd9ea675a8a
teng <wwwsousoucom@126.com>
2020-05-09 15:09:42 +0800

    first

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# chip8 rust game
+# Basic Rust
+> There are no strict constraints on which part of the object store the object can access, because there are no restrictions on the way the object references are passed around.
+
+This has repercussions when preventing representation exposure for aggregate objects.
+
+A better solution can be to restrict the visibility of different types of objects that are created. This is done by saying that all objects have a context associated with them. All paths from the root of the system must pass though the objects' owner.
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,3 @@
 fn main() {
-    let use_closure = || {
-        println!("This is a closure!");
-    };
-    use_closure();
-    println!("Hello world!");
-    // 闭包定义可以为每个参数和返回值类型推导一个具体的类型，但是不能推导两次
-    let add_one_v2 = |x:u32| -> u32 { x+1 };
-    let add_one_v3 = |x| {x+1};
-    let add_one_v4 = |x| x+1;
 
-    let a = add_one_v1(5);
-    let b = add_one_v2(5);
-    let c = add_one_v3(5);
-    let d = add_one_v4(5);
-    println!("a={} b={} c={} d={}", a, b, c, d);
-    // 不能推导两次的例子
-    let example_closure = |x|x;
-    let s = example_closure(String::from("hello"));
-    println!("s={}", s);
-    let n = example_closure(5);
-    println!("s={}", s);// 报错
-}
-
-// 语法格式
-fn add_one_v1(x: u32) -> u32 {
-    x+1
 }
