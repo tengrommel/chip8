1d5433607f6c71d565b8a07f0a3c6c26f6a770c6
teng <wwwsousoucom@126.com>
2020-05-15 12:38:28 +0800

    join await

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
+use async_std::task::block_on;
+use std::{thread, time};
+use futures::future::join;
 
+async fn hello() {
+    println!("Hello");
+}
+
+async fn connect_db() -> String {
+    thread::sleep(time::Duration::from_secs(1));
+    String::from("db client")
+}
+
+async fn connect_redis() -> String {
+    thread::sleep(time::Duration::from_secs(1));
+    String::from("redis client")
+}
+
+async fn main_executor() {
+    hello().await;
+    let (db, redis) = join(connect_db(), connect_redis()).await;
+    println!("{} {}", db, redis);
+}
+
+fn main() {
+    let now = time::Instant::now();
+    block_on(main_executor());
+    println!("executed in {:?}", now.elapsed());
 }
