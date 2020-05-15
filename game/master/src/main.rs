a8dd64de5ae59b8a7e4c5561cfe95a37b01777af
teng <wwwsousoucom@126.com>
2020-05-15 10:52:17 +0800

    game

--- game/master/src/main.rs
+++ game/master/src/main.rs
@@ -1,29 +1,47 @@
+mod my_mod {
+    // Items in modules default to private visibility.
+    fn private_function() {
+        println!("called `my_mod::private_function()`");
+    }
+    pub fn function() {
+        println!("called `my_mod::function()`");
+    }
+    pub fn indirect_access() {
+        println!("called `my_mod::indirect_access()`, that\n>");
+        private_function();
+    }
+    pub mod nested {
+        pub fn function() {
+            println!("called `my_mod::nested::function()`");
+        }
+
+        #[allow(dead_code)]
+        fn private_function() {
+            println!("called `my_mod::nested::private_function()`");
+        }
+
+        // Functions declared using `pub(in path)` syntax are only visible
+        // within the given path. `path` must be a parent or ancestor module
+        pub(in crate::my_mod) fn public_function_in_my_mod() {
+            print!("called `my_mod::nested::public_function_in_my_mod()`, that\n> ");
+            public_function_in_nested();
+        }
+
+        // Functions declared using `pub(self)` syntax are only visible within
+        // the current module, which is the same as leaving them private
+        pub(self) fn public_function_in_nested() {
+            println!("called `my_mod::nested::public_function_in_nested()`");
+        }
+
+        // Functions declared using `pub(super)` syntax are only visible within
+        // the parent module
+        pub(super) fn public_function_in_super_mod() {
+            println!("called `my_mod::nested::public_function_in_super_mod()`");
+        }
+    }
+}
+
+
 fn main() {
-    fizzbuzz_to(100);
-}
 
-// Function that returns a boolean value
-fn is_divisible_by(lhs: u32, rhs: u32) -> bool {
-    if rhs == 0 {
-        return false
-    }
-    lhs % rhs == 0
-}
-
-fn fizzbuzz(n: u32) -> () {
-    if is_divisible_by(n, 15) {
-        println!("fizzbuzz");
-    } else if is_divisible_by(n, 3) {
-        println!("fizz");
-    } else if is_divisible_by(n, 5) {
-        println!("buzz");
-    } else {
-        println!("{}", n);
-    }
-}
-
-fn fizzbuzz_to(n: u32) {
-    for n in 1..n + 1 {
-        fizzbuzz(n);
-    }
 }
