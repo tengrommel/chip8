006bb57eba3470bd81b1fd7f79af99d0b76c81ca
teng <wwwsousoucom@126.com>
2020-06-16 23:48:52 +0800

    find

--- game/master/src/main.rs
+++ game/master/src/main.rs
@@ -7,6 +7,7 @@ use amethyst::{
         types::DefaultBackend,
         RenderingBundle,
     },
+    ui::{RenderUi, UiBundle},
     utils::application_root_dir,
 };
 
@@ -34,9 +35,18 @@ fn main() -> amethyst::Result<()> {
         )?
         .with_bundle(TransformBundle::new())?
         .with_bundle(input_bundle)?
-        .with(systems::PaddleSystem, "paddle_system", &["input_system"]);
+        .with(systems::PaddleSystem, "paddle_system", &["input_system"])
+        .with(systems::MoveBallsSystem, "ball_system", &[])
+        .with(
+            systems::BounceSystem,
+            "collision_system",
+            &["paddle_system", "ball_system"],
+        )
+        .with(systems::WinnerSystem, "winner_system", &["ball_system"])
+        .with_bundle(RenderingBundle::<DefaultBackend>::new().with_plugin(RenderUi::default()))?
+        .with_bundle(UiBundle::<StringBindings>::new())?;
     let assets_dir = app_root.join("assets");
-    let mut game = Application::new(assets_dir, Pong, game_data)?;
+    let mut game = Application::new(assets_dir, Pong::default(), game_data)?;
     game.run();
     Ok(())
 }
