397e41d7f7c50da67adcb9436641e22e8e6d18a7
teng <wwwsousoucom@126.com>
2020-06-16 23:55:00 +0800

    over

--- game/master/src/main.rs
+++ game/master/src/main.rs
@@ -18,23 +18,24 @@ mod systems;
 
 fn main() -> amethyst::Result<()> {
     amethyst::start_logger(Default::default());
+
     let app_root = application_root_dir()?;
-    let display_config_path = app_root.join("config").join("display.ron");
-    let binding_path = app_root.join("config").join("bindings.ron");
-    let input_bundle =
-        InputBundle::<StringBindings>::new().with_bindings_from_file(binding_path)?;
+    let display_config_path = app_root.join("config/display.ron");
+
+    // This line is not mentioned in the pong tutorial as it is specific to the context
+    // of the git repository. It only is a different location to load the assets from.
+    let assets_dir = app_root.join("assets");
+
     let game_data = GameDataBuilder::default()
-        .with_bundle(
-            RenderingBundle::<DefaultBackend>::new()
-                .with_plugin(
-                    RenderToWindow::from_config_path(display_config_path)?
-                        .with_clear([0.0, 0.0, 0.0, 1.0]),
-                )
-                // RenderFlat2D plugin is used to render entities with a `SpriteRender` component.
-                .with_plugin(RenderFlat2D::default()),
-        )?
+        // Add the transform bundle which handles tracking entity positions
         .with_bundle(TransformBundle::new())?
-        .with_bundle(input_bundle)?
+        .with_bundle(
+            InputBundle::<StringBindings>::new().with_bindings_from_file(
+                app_root.join("config/bindings.ron"),
+            )?,
+        )?
+        .with_bundle(UiBundle::<StringBindings>::new())?
+        // We have now added our own systems, defined in the systems module
         .with(systems::PaddleSystem, "paddle_system", &["input_system"])
         .with(systems::MoveBallsSystem, "ball_system", &[])
         .with(
@@ -43,9 +44,19 @@ fn main() -> amethyst::Result<()> {
             &["paddle_system", "ball_system"],
         )
         .with(systems::WinnerSystem, "winner_system", &["ball_system"])
-        .with_bundle(RenderingBundle::<DefaultBackend>::new().with_plugin(RenderUi::default()))?
-        .with_bundle(UiBundle::<StringBindings>::new())?;
-    let assets_dir = app_root.join("assets");
+        .with_bundle(
+            RenderingBundle::<DefaultBackend>::new()
+                // The RenderToWindow plugin provides all the scaffolding for opening a window and
+                // drawing on it
+                .with_plugin(
+                    RenderToWindow::from_config_path(display_config_path)?
+                        .with_clear([0.0, 0.0, 0.0, 1.0]),
+                )
+                // RenderFlat2D plugin is used to render entities with `SpriteRender` component.
+                .with_plugin(RenderFlat2D::default())
+                .with_plugin(RenderUi::default()),
+        )?;
+
     let mut game = Application::new(assets_dir, Pong::default(), game_data)?;
     game.run();
     Ok(())
